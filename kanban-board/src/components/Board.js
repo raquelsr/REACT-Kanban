688fee337e67e81b4450cdbafc87309d6682973f
raquelsr <raquelserranor@hotmail.com>
2022-02-23 23:38:13 +0100

    Dnd in columns

--- kanban-board/src/components/Board.js
+++ kanban-board/src/components/Board.js
@@ -2,6 +2,7 @@ import { Box } from '@mui/system';
 import { useState, useEffect } from 'react';
 import { Column } from './Column';
 import { DragDropContext } from 'react-beautiful-dnd';
+import { Droppable } from 'react-beautiful-dnd';
 
 const API_URL = 'http://localhost:3001';
 
@@ -29,7 +30,7 @@ export const Board = () => {
   }, []);
 
   const onDragEnd = (result) => {
-    const { destination, source, draggableId } = result;
+    const { destination, source, draggableId, type } = result;
     if (!destination) {
       return;
     }
@@ -41,63 +42,76 @@ export const Board = () => {
       return;
     }
 
-    const startColumn = columnList.find(
-      (column) => column.id === source.droppableId
-    );
-
-    const finishColumn = columnList.find(
-      (column) => column.id === destination.droppableId
-    );
-
-    if (startColumn === finishColumn) {
-      const task = startColumn.taskIdList.find((task) => task === draggableId);
-      const newTaskList = Array.from(startColumn.taskIdList);
-      newTaskList.splice(source.index, 1);
-      newTaskList.splice(destination.index, 0, task);
-
-      const newColumn = {
-        ...startColumn,
-        taskIdList: newTaskList,
-      };
-
-      const index = columnList.findIndex(
-        (column) => column.id === newColumn.id
-      );
-      const newColumnList = Array.from(columnList);
-      newColumnList.splice(index, 1);
-      newColumnList.splice(index, 0, newColumn);
+    if (type === 'column') {
+      const column = columnList.find((column) => column.id === draggableId);
+      const newColumnList = [...columnList]; //ES6
+      newColumnList.splice(source.index, 1);
+      newColumnList.splice(destination.index, 0, column);
 
       setColumnList(newColumnList);
-      // Update API
     } else {
-      const task = startColumn.taskIdList.find((task) => task === draggableId);
-      const newTaskListStart = Array.from(startColumn.taskIdList);
-      newTaskListStart.splice(source.index, 1);
-      const newColumnStart = {
-        ...startColumn,
-        taskIdList: newTaskListStart,
-      };
-
-      const newTaskListFinish = Array.from(finishColumn.taskIdList);
-      newTaskListFinish.splice(destination.index, 0, task);
-      const newColumnFinish = {
-        ...finishColumn,
-        taskIdList: newTaskListFinish,
-      };
-
-      const indexStart = columnList.findIndex(
-        (column) => column.id === newColumnStart.id
+      const startColumn = columnList.find(
+        (column) => column.id === source.droppableId
       );
-      const indexFinish = columnList.findIndex(
-        (column) => column.id === newColumnFinish.id
-      );
-      const newColumnList = Array.from(columnList);
-      newColumnList.splice(indexStart, 1);
-      newColumnList.splice(indexStart, 0, newColumnStart);
-      newColumnList.splice(indexFinish, 1);
-      newColumnList.splice(indexFinish, 0, newColumnFinish);
 
-      setColumnList(newColumnList);
+      const finishColumn = columnList.find(
+        (column) => column.id === destination.droppableId
+      );
+
+      if (startColumn === finishColumn) {
+        const task = startColumn.taskIdList.find(
+          (task) => task === draggableId
+        );
+        const newTaskList = Array.from(startColumn.taskIdList);
+        newTaskList.splice(source.index, 1);
+        newTaskList.splice(destination.index, 0, task);
+
+        const newColumn = {
+          ...startColumn,
+          taskIdList: newTaskList,
+        };
+
+        const index = columnList.findIndex(
+          (column) => column.id === newColumn.id
+        );
+        const newColumnList = Array.from(columnList);
+        newColumnList.splice(index, 1);
+        newColumnList.splice(index, 0, newColumn);
+
+        setColumnList(newColumnList);
+        // Update API
+      } else {
+        const task = startColumn.taskIdList.find(
+          (task) => task === draggableId
+        );
+        const newTaskListStart = Array.from(startColumn.taskIdList);
+        newTaskListStart.splice(source.index, 1);
+        const newColumnStart = {
+          ...startColumn,
+          taskIdList: newTaskListStart,
+        };
+
+        const newTaskListFinish = Array.from(finishColumn.taskIdList);
+        newTaskListFinish.splice(destination.index, 0, task);
+        const newColumnFinish = {
+          ...finishColumn,
+          taskIdList: newTaskListFinish,
+        };
+
+        const indexStart = columnList.findIndex(
+          (column) => column.id === newColumnStart.id
+        );
+        const indexFinish = columnList.findIndex(
+          (column) => column.id === newColumnFinish.id
+        );
+        const newColumnList = Array.from(columnList);
+        newColumnList.splice(indexStart, 1);
+        newColumnList.splice(indexStart, 0, newColumnStart);
+        newColumnList.splice(indexFinish, 1);
+        newColumnList.splice(indexFinish, 0, newColumnFinish);
+
+        setColumnList(newColumnList);
+      }
     }
   };
 
@@ -116,11 +130,27 @@ export const Board = () => {
     >
       <h1>Kanban Board</h1>
       <DragDropContext onDragEnd={onDragEnd}>
-        <Box sx={{ display: 'flex', justifyContent: 'space-around' }}>
-          {columnList.map((column) => (
-            <Column key={column.id} column={column} taskList={taskList} />
-          ))}
-        </Box>
+        <Droppable
+          droppableId="all-columns"
+          direction="horizontal"
+          type="column"
+        >
+          {(provided) => (
+            <div {...provided.droppableProps} ref={provided.innerRef}>
+              <Box sx={{ display: 'flex', justifyContent: 'space-around' }}>
+                {columnList.map((column, index) => (
+                  <Column
+                    key={column.id}
+                    column={column}
+                    taskList={taskList}
+                    index={index}
+                  />
+                ))}
+                {provided.placeholder}
+              </Box>
+            </div>
+          )}
+        </Droppable>
       </DragDropContext>
     </Box>
   );
--- kanban-board/src/components/Column.js
+++ kanban-board/src/components/Column.js
@@ -1,9 +1,9 @@
 import { Task } from './Task';
 import { Box } from '@mui/system';
 import { InputContainer } from './InputContainer';
-import { Droppable } from 'react-beautiful-dnd';
+import { Droppable, Draggable } from 'react-beautiful-dnd';
 
-export const Column = ({ column, taskList }) => {
+export const Column = ({ column, taskList, index }) => {
   const taskValues = column.taskIdList.reduce((value, taskId) => {
     const task = taskList.find((task) => task.id === taskId);
     if (task) value.push(task);
@@ -12,30 +12,40 @@ export const Column = ({ column, taskList }) => {
 
   const dataColumn = { ...column, tasks: taskValues };
   return (
-    <Box
-      sx={{
-        background: '#E5E7E9',
-        px: 3,
-        py: 1,
-        borderRadius: 3,
-        border: '1px solid black',
-        '& h3': {
-          textTransform: 'uppercase',
-        },
-      }}
-    >
-      <h3>{column.title}</h3>
-      <Droppable droppableId={column.id}>
-        {(provided) => (
-          <div ref={provided.innerRef} {...provided.droppableProps}>
-            {dataColumn.tasks.map((task, index) => (
-              <Task key={task.id} task={task} index={index} />
-            ))}
-            {provided.placeholder}
-          </div>
-        )}
-      </Droppable>
-      <InputContainer />
-    </Box>
+    <Draggable draggableId={column.id} index={index}>
+      {(provided) => (
+        <div
+          {...provided.dragHandleProps}
+          {...provided.draggableProps}
+          ref={provided.innerRef}
+        >
+          <Box
+            sx={{
+              background: '#E5E7E9',
+              px: 3,
+              py: 1,
+              borderRadius: 3,
+              border: '1px solid black',
+              '& h3': {
+                textTransform: 'uppercase',
+              },
+            }}
+          >
+            <h3>{column.title}</h3>
+            <Droppable droppableId={column.id} type="task">
+              {(provided) => (
+                <div ref={provided.innerRef} {...provided.droppableProps}>
+                  {dataColumn.tasks.map((task, index) => (
+                    <Task key={task.id} task={task} index={index} />
+                  ))}
+                  {provided.placeholder}
+                </div>
+              )}
+            </Droppable>
+            <InputContainer />
+          </Box>
+        </div>
+      )}
+    </Draggable>
   );
 };
